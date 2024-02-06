86ffa178fb22a7fe7cebcce3ad07e2c67d20ef39
Giorgio Garofalo <giogar2705@gmail.com>
2024-02-06 22:55:01 +0100

    Init main

---
+++ src/main/kotlin/eu/iamgio/quarkdown/ExitCodes.kt
@@ -0,0 +1,5 @@
+package eu.iamgio.quarkdown
+
+const val NO_SOURCE_FILE_EXIT_CODE = 64
+const val COMPILE_ERROR_EXIT_CODE = 65
+const val RUNTIME_ERROR_EXIT_CODE = 70
--- src/main/kotlin/eu/iamgio/quarkdown/Quarkdown.kt
+++ src/main/kotlin/eu/iamgio/quarkdown/Quarkdown.kt
@@ -1,5 +1,14 @@
 package eu.iamgio.quarkdown
 
-fun main(args: Array<String>) {
+import eu.iamgio.quarkdown.log.Log
+import java.io.File
+import kotlin.system.exitProcess
 
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        Log.error("No source file passed.")
+        exitProcess(NO_SOURCE_FILE_EXIT_CODE)
+    }
+
+    val sourceFile = File(args.first())
 }
