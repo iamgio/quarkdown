78b533922e51b877b9d70a6604888f0902614611
Giorgio Garofalo <giogar2705@gmail.com>
2024-02-07 01:25:53 +0100

    Add documentation to ExitCodes

--- src/main/kotlin/eu/iamgio/quarkdown/ExitCodes.kt
+++ src/main/kotlin/eu/iamgio/quarkdown/ExitCodes.kt
@@ -1,5 +1,16 @@
 package eu.iamgio.quarkdown
 
+/**
+ * Exit code when a source file was not supplied.
+ */
 const val NO_SOURCE_FILE_EXIT_CODE = 64
+
+/**
+ * Exit code when a compile-time error occurs.
+ */
 const val COMPILE_ERROR_EXIT_CODE = 65
+
+/**
+ * Exit code when a runtime error occurs.
+ */
 const val RUNTIME_ERROR_EXIT_CODE = 70
