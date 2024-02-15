ada72a420bb0207d26bb5d544c3d63c5956ef943
Giorgio Garofalo <giogar2705@gmail.com>
2024-02-15 14:32:01 +0100

    Generalize TokenWrapper#parse into #accept

--- core/src/main/kotlin/eu/iamgio/quarkdown/lexer/BlockTokenWrappers.kt
+++ core/src/main/kotlin/eu/iamgio/quarkdown/lexer/BlockTokenWrappers.kt
@@ -1,87 +1,87 @@
 package eu.iamgio.quarkdown.lexer
 
-import eu.iamgio.quarkdown.parser.BlockTokenParser
+import eu.iamgio.quarkdown.common.BlockTokenVisitor
 
 /**
  * @see eu.iamgio.quarkdown.ast.Newline
  */
 class NewlineToken(token: Token) : TokenWrapper(token) {
-    override fun parse(parser: BlockTokenParser) = parser.visit(this)
+    override fun <O> accept(visitor: BlockTokenVisitor<O>) = visitor.visit(this)
 }
 
 /**
  * @see eu.iamgio.quarkdown.ast.BlockCode
  */
 class BlockCodeToken(token: Token) : TokenWrapper(token) {
-    override fun parse(parser: BlockTokenParser) = parser.visit(this)
+    override fun <O> accept(visitor: BlockTokenVisitor<O>) = visitor.visit(this)
 }
 
 /**
  * @see eu.iamgio.quarkdown.ast.Newline
  */
 class FencesCodeToken(token: Token) : TokenWrapper(token) {
-    override fun parse(parser: BlockTokenParser) = parser.visit(this)
+    override fun <O> accept(visitor: BlockTokenVisitor<O>) = visitor.visit(this)
 }
 
 /**
  * @see eu.iamgio.quarkdown.ast.HorizontalRule
  */
 class HorizontalRuleToken(token: Token) : TokenWrapper(token) {
-    override fun parse(parser: BlockTokenParser) = parser.visit(this)
+    override fun <O> accept(visitor: BlockTokenVisitor<O>) = visitor.visit(this)
 }
 
 /**
  * @see eu.iamgio.quarkdown.ast.Heading
  */
 class HeadingToken(token: Token) : TokenWrapper(token) {
-    override fun parse(parser: BlockTokenParser) = parser.visit(this)
+    override fun <O> accept(visitor: BlockTokenVisitor<O>) = visitor.visit(this)
 }
 
 /**
  * @see eu.iamgio.quarkdown.ast.Heading
  */
 class SetextHeadingToken(token: Token) : TokenWrapper(token) {
-    override fun parse(parser: BlockTokenParser) = parser.visit(this)
+    override fun <O> accept(visitor: BlockTokenVisitor<O>) = visitor.visit(this)
 }
 
 /**
  * @see eu.iamgio.quarkdown.ast.LinkDefinition
  */
 class LinkDefinitionToken(token: Token) : TokenWrapper(token) {
-    override fun parse(parser: BlockTokenParser) = parser.visit(this)
+    override fun <O> accept(visitor: BlockTokenVisitor<O>) = visitor.visit(this)
 }
 
 /**
  * @see eu.iamgio.quarkdown.ast.ListItem
  */
 class ListItemToken(token: Token) : TokenWrapper(token) {
-    override fun parse(parser: BlockTokenParser) = parser.visit(this)
+    override fun <O> accept(visitor: BlockTokenVisitor<O>) = visitor.visit(this)
 }
 
 /**
  * @see eu.iamgio.quarkdown.ast.Html
  */
 class HtmlToken(token: Token) : TokenWrapper(token) {
-    override fun parse(parser: BlockTokenParser) = parser.visit(this)
+    override fun <O> accept(visitor: BlockTokenVisitor<O>) = visitor.visit(this)
 }
 
 /**
  * @see eu.iamgio.quarkdown.ast.Paragraph
  */
 class ParagraphToken(token: Token) : TokenWrapper(token) {
-    override fun parse(parser: BlockTokenParser) = parser.visit(this)
+    override fun <O> accept(visitor: BlockTokenVisitor<O>) = visitor.visit(this)
 }
 
 /**
  * @see eu.iamgio.quarkdown.ast.BlockQuote
  */
 class BlockQuoteToken(token: Token) : TokenWrapper(token) {
-    override fun parse(parser: BlockTokenParser) = parser.visit(this)
+    override fun <O> accept(visitor: BlockTokenVisitor<O>) = visitor.visit(this)
 }
 
 /**
  * @see eu.iamgio.quarkdown.ast.BlockText
  */
 class BlockTextToken(token: Token) : TokenWrapper(token) {
-    override fun parse(parser: BlockTokenParser) = parser.visit(this)
+    override fun <O> accept(visitor: BlockTokenVisitor<O>) = visitor.visit(this)
 }
--- core/src/main/kotlin/eu/iamgio/quarkdown/lexer/TokenWrapper.kt
+++ core/src/main/kotlin/eu/iamgio/quarkdown/lexer/TokenWrapper.kt
@@ -1,6 +1,7 @@
 package eu.iamgio.quarkdown.lexer
 
 import eu.iamgio.quarkdown.ast.Node
+import eu.iamgio.quarkdown.common.BlockTokenVisitor
 import eu.iamgio.quarkdown.parser.BlockTokenParser
 
 /**
@@ -8,5 +9,16 @@ import eu.iamgio.quarkdown.parser.BlockTokenParser
  * @param data the wrapped token
  */
 sealed class TokenWrapper(val data: Token) {
-    abstract fun parse(parser: BlockTokenParser): Node // TODO change to general TokenParser
+    /**
+     * Parses this token into an AST [Node].
+     * @param parser parser to delegate the parsing process to
+     */
+    fun parse(parser: BlockTokenParser): Node = this.accept(parser)
+
+    /**
+     * Accepts a visitor.
+     * @param O output type of the visitor
+     * @return output of the visit
+     */
+    abstract fun <O> accept(visitor: BlockTokenVisitor<O>): O // TODO change to general TokenParser
 }
