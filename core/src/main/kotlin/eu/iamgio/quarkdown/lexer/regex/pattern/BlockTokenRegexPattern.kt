0ecf61aed1081df1c46d1a3079cfb1bd4965b217
Giorgio Garofalo <giogar2705@gmail.com>
2024-02-23 19:01:41 +0100

    Rename BlockTokenRegexPattern.LIST to LISTITEM

--- core/src/main/kotlin/eu/iamgio/quarkdown/lexer/regex/pattern/BlockTokenRegexPattern.kt
+++ core/src/main/kotlin/eu/iamgio/quarkdown/lexer/regex/pattern/BlockTokenRegexPattern.kt
@@ -76,7 +76,7 @@ enum class BlockTokenRegexPattern(
             )
             .build(),
     ),
-    LIST_ITEM(
+    LISTITEM(
         ::ListItemToken,
         RegexBuilder("^( {0,3}bullet)(?!bullet)[ \\t]?(((.+(\\n(?!(\\s+\\n| {0,3}bullet)))?)*((\\s*\\n)( {2,}))*)*)")
             .withReference("bullet", BULLET_HELPER)
