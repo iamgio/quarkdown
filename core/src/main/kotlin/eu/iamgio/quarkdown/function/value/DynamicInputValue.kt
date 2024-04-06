2f8ca7e86bfa4c953ac3e569219599f552bbe17e
Giorgio Garofalo <giogar2705@gmail.com>
2024-04-06 14:38:34 +0200

    Automate dynamic type conversion

--- core/src/main/kotlin/eu/iamgio/quarkdown/function/value/DynamicInputValue.kt
+++ core/src/main/kotlin/eu/iamgio/quarkdown/function/value/DynamicInputValue.kt
@@ -1,6 +1,8 @@
 package eu.iamgio.quarkdown.function.value
 
 import kotlin.reflect.KClass
+import kotlin.reflect.full.declaredFunctions
+import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.isSubclassOf
 
 /**
@@ -12,10 +14,24 @@ data class DynamicInputValue(override val unwrappedValue: String) : InputValue<S
      * @param type type of the value to convert this automatic value to
      * @return a new typed [InputValue], automatically determined from [type]
      */
-    fun convertTo(type: KClass<out InputValue<*>>): InputValue<*> =
-        when {
-            type.isSubclassOf(String::class) -> ValueFactory.string(unwrappedValue)
-            type.isSubclassOf(Number::class) -> ValueFactory.number(unwrappedValue)
-            else -> throw IllegalArgumentException("Cannot convert DynamicInputValue to type $type")
+    fun convertTo(type: KClass<out InputValue<*>>): InputValue<*> {
+        // Gets ValueFactory methods annotated with @FromDynamicType(X::class),
+        // and the one with a matching type is invoked.
+        for (function in ValueFactory::class.declaredFunctions) {
+            val from = function.findAnnotation<FromDynamicType>() ?: continue
+            if (type.isSubclassOf(from.unwrappedType)) {
+                return function.call(ValueFactory, unwrappedValue) as InputValue<*>
+            }
         }
+
+        throw IllegalArgumentException("Cannot convert DynamicInputValue to type $type")
+    }
 }
+
+/**
+ * When a [ValueFactory] method is marked with this annotation, it is a candidate for type conversion from a [DynamicInputValue].
+ * @param unwrappedType when an object matches this type, the function is suitable for invocation
+ * @see ValueFactory
+ */
+@Target(AnnotationTarget.FUNCTION)
+annotation class FromDynamicType(val unwrappedType: KClass<*>)
--- core/src/main/kotlin/eu/iamgio/quarkdown/function/value/ValueFactory.kt
+++ core/src/main/kotlin/eu/iamgio/quarkdown/function/value/ValueFactory.kt
@@ -4,7 +4,9 @@ package eu.iamgio.quarkdown.function.value
  * Factory of [Value] wrappers from raw string data.
  */
 object ValueFactory {
+    @FromDynamicType(String::class)
     fun string(raw: String) = StringValue(raw)
 
-    fun number(raw: String) = NumberValue(raw.toIntOrNull() ?: raw.toFloatOrNull() ?: 0)
+    @FromDynamicType(Number::class)
+    fun number(raw: String) = NumberValue(raw.toIntOrNull() ?: raw.toFloatOrNull() ?: Double.NaN)
 }
