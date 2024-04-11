2ec24972f8bbb0bc4576ad648ae8ac0dd63f0548
Giorgio Garofalo <giogar2705@gmail.com>
2024-04-11 17:41:50 +0200

    Add missing docs

--- core/src/main/kotlin/eu/iamgio/quarkdown/function/error/UnresolvedReferenceException.kt
+++ core/src/main/kotlin/eu/iamgio/quarkdown/function/error/UnresolvedReferenceException.kt
@@ -4,6 +4,7 @@ import eu.iamgio.quarkdown.UNRESOLVED_REFERENCE_EXIT_CODE
 import eu.iamgio.quarkdown.pipeline.error.PipelineException
 
 /**
- *
+ * An exception thrown when a function call does not reference any registered function declaration.
+ * @param symbol function name
  */
 class UnresolvedReferenceException(symbol: String) : PipelineException("Unresolved reference: $symbol", UNRESOLVED_REFERENCE_EXIT_CODE)
