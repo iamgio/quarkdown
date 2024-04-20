23a32485293556ee6e0258da2aec518439bf56b1
Giorgio Garofalo <giogar2705@gmail.com>
2024-04-20 18:25:28 +0200

    Add .code test

---
+++ stdlib/src/test/kotlin/eu/iamgio/quarkdown/stdlib/TextTest.kt
@@ -0,0 +1,31 @@
+package eu.iamgio.quarkdown.stdlib
+
+import eu.iamgio.quarkdown.ast.Code
+import eu.iamgio.quarkdown.ast.MarkdownContent
+import eu.iamgio.quarkdown.ast.Text
+import eu.iamgio.quarkdown.context.MutableContext
+import eu.iamgio.quarkdown.flavor.quarkdown.QuarkdownFlavor
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+
+/**
+ * [Text] module tests.
+ */
+class TextTest {
+    @Test
+    fun `markdown code`() {
+        val code =
+            code(
+                MutableContext(QuarkdownFlavor),
+                "kotlin",
+                MarkdownContent(listOf(Text("fun foo() = 1"))),
+            )
+
+        val node = code.unwrappedValue
+
+        assertIs<Code>(node)
+        assertEquals("fun foo() = 1", node.content)
+        assertEquals("kotlin", node.language)
+    }
+}
