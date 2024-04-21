8c3949fa66c6c6a7a9d6355880cbe347c5d7413a
Giorgio Garofalo <giogar2705@gmail.com>
2024-04-21 12:48:49 +0200

    Use `partition` to split injected and regular parameters

--- core/src/main/kotlin/eu/iamgio/quarkdown/function/call/FunctionArgumentsLinker.kt
+++ core/src/main/kotlin/eu/iamgio/quarkdown/function/call/FunctionArgumentsLinker.kt
@@ -21,17 +21,15 @@ class FunctionArgumentsLinker(private val call: FunctionCall<*>) {
     lateinit var links: Links
 
     /**
+     * @param parameters regular (non-injected) parameters of the function
      * @return the parameter-argument pairs for regular user-supplied arguments
      */
-    private fun generateRegularLinks(): Links =
+    private fun generateRegularLinks(parameters: List<FunctionParameter<*>>): Links =
         buildMap {
-            // Injected parameters are handled in generateInjectedLinks.
-            val regularParameters = call.function.parameters.filterNot { it.isInjected }
-
             call.arguments.forEachIndexed { index, argument ->
                 // Corresponding parameter.
                 // Error if args count > params count.
-                val parameter = regularParameters.getOrNull(index) ?: throw InvalidArgumentCountException(call)
+                val parameter = parameters.getOrNull(index) ?: throw InvalidArgumentCountException(call)
 
                 // The type of dynamic arguments is determined.
                 val staticArgument =
@@ -64,11 +62,12 @@ class FunctionArgumentsLinker(private val call: FunctionCall<*>) {
         }
 
     /**
+     * @param parameters injected parameters of the function
      * @return the parameter-argument pairs for automatically injected values
      * @see Injected
      * @see FunctionParameter.isInjected
      */
-    private fun generateInjectedLinks(): Links =
+    private fun generateInjectedLinks(parameters: List<FunctionParameter<*>>): Links =
         buildMap {
             // Non-injected parameters are handled in generateRegularLinks.
             val injectedParameters = call.function.parameters.filter { it.isInjected }
@@ -85,7 +84,11 @@ class FunctionArgumentsLinker(private val call: FunctionCall<*>) {
      *         if there is a mismatch between arguments and parameters
      */
     fun link() {
-        this.links = generateRegularLinks() + generateInjectedLinks()
+        // Injected and non-injected parameters are handled separately.
+        val (injected, regular) = call.function.parameters.partition { it.isInjected }
+
+        // Argument-parameter links are generated for both types of parameters and joined together.
+        this.links = generateRegularLinks(regular) + generateInjectedLinks(injected)
 
         call.function.parameters.forEach { parameter ->
             // If mandatory params count > args count.
@@ -100,6 +103,7 @@ class FunctionArgumentsLinker(private val call: FunctionCall<*>) {
      * @param T type of the value
      * @return the value of the argument by the given name
      * @throws NoSuchElementException if [name] does not match any parameter name
+     * @deprecated used in tests only
      */
     inline fun <reified T> arg(name: String): T =
         this.links.entries
