99385f97e0e562dcbb2b3eb1001b8c32810ed97e
Giorgio Garofalo <giogar2705@gmail.com>
2024-08-18 17:29:08 +0200

    Beaver/beamer: "elements" section

---
+++ oop-demo/code/2.java
@@ -0,0 +1,13 @@
+String s = new String("just a string");
+
+// Un caso ad-hoc di Java, equivalente a:
+// String s2 = new String("home");
+String s2 = "home";
+
+// Definisco il nome di s4, ma non lo inizializzo
+String s4;
+// ora assegno s4, da qui in poi è utilizzabile
+s4 = "car";
+
+// Il nome i è assegnato al valore primitivo 5
+int i = 5;
---
+++ oop-demo/code/3.java
@@ -0,0 +1,23 @@
+int i = 5; // Assegnamenti con tipi primitivi
+double d = 145e-20;
+boolean b = true;
+int other = i;
+
+Object o = new Object(); // Assegnamenti con classi
+String s = "another string";
+Point2D p = new Point2D(10.4, 20.3);
+Point2D q = p; // Assegnamento da/a variabile
+
+Object on = null; // Assegnamento a null
+
+---
+
+var i = 5; // Assegnamenti con tipi primitivi
+var d = 145e-20;
+var b = true;
+var other = i;
+
+var o = new Object(); // Assegnamenti con classi
+var s = "another string";
+var p = new Point2D(10.4, 20.3);
+var q = p; // Assegnamento da/a variabile
