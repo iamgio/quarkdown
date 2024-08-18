4a976181a44287639e21b70aa8cdf07fc9b139b9
Giorgio Garofalo <giogar2705@gmail.com>
2024-08-18 16:56:40 +0200

    OOP demo: set up

---
+++ oop-demo/code/1.java
@@ -0,0 +1,11 @@
+// Creo un oggetto String, e lo assegno al nome s
+// questo oggetto stringa rappresenta la sequenza
+// vuota di caratteri
+String s = new String();
+
+// In generale si crea un oggetto con l’operatore <new>
+// Accetta 0, 1 o più argomenti, a seconda del tipo:
+// valori primitivi (numeri), string literals, o oggetti
+String s2 = new String("just a string");
+Point2D p = new Point2D(10.5, 20.3);
+Object o = new Object();
