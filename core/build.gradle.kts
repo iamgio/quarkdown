b2ce1cee45761b48f872bc5fac60f4d3884b6d72
Giorgio Garofalo <giogar2705@gmail.com>
2024-08-21 23:10:54 +0200

    Bump log4j version

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
 dependencies {
     implementation(kotlin("reflect"))
     testImplementation(kotlin("test"))
-    implementation("org.apache.logging.log4j:log4j-core:2.22.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
     implementation("org.apache.commons:commons-text:1.11.0")
 }
