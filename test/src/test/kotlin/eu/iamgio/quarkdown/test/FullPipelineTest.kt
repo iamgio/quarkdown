7f138cc0f9a6c66d9354fac1049cfee76bd8642f
Giorgio Garofalo <giogar2705@gmail.com>
2024-08-27 17:28:58 +0200

    Use media storage in FullPipelineTest

--- test/src/test/kotlin/eu/iamgio/quarkdown/test/FullPipelineTest.kt
+++ test/src/test/kotlin/eu/iamgio/quarkdown/test/FullPipelineTest.kt
@@ -951,4 +951,76 @@ class FullPipelineTest {
             )
         }
     }
+
+    @Test
+    fun `media storage`() {
+        execute(
+            """
+            This is the Quarkdown logo: ![Quarkdown](img/icon.png).                                 
+            """.trimIndent(),
+            enableMediaStorage = false,
+        ) {
+            assertEquals("<p>This is the Quarkdown logo: <img src=\"img/icon.png\" alt=\"Quarkdown\" />.</p>", it)
+            assertEquals(0, mediaStorage.all.size)
+        }
+
+        execute(
+            """
+            This is the Quarkdown logo: ![Quarkdown](img/icon.png).                                 
+            """.trimIndent(),
+            enableMediaStorage = true,
+        ) {
+            assertEquals("<p>This is the Quarkdown logo: <img src=\"media/icon", it.toString().substringBefore("-"))
+            // The file name is "media/icon-[encoded].png"
+            assertEquals("\" alt=\"Quarkdown\" />.</p>", it.toString().substringAfter(".png"))
+        }
+
+        execute(
+            """
+            .center
+                ![Icon](https://raw.githubusercontent.com/iamgio/quarkdown/project-files/images/ticon-light.svg "The Quarkdown icon")
+                
+                ![Banner](https://raw.githubusercontent.com/iamgio/quarkdown/project-files/images/tbanner-light.svg)
+            """.trimIndent(),
+            options = MutableContextOptions(enableRemoteMediaStorage = true),
+            enableMediaStorage = true,
+        ) {
+            assertEquals(
+                "<div class=\"align align-center\">" +
+                    "<p>" +
+                    "<figure>" +
+                    "<img src=\"media/https-raw.githubusercontent.com-iamgio-quarkdown-project-files-images-ticon-light.svg\" " +
+                    "alt=\"Icon\" title=\"The Quarkdown icon\" />" +
+                    "<figcaption>The Quarkdown icon</figcaption>" +
+                    "</figure>" +
+                    "</p>" +
+                    "<p>" +
+                    "<img src=\"media/https-raw.githubusercontent.com-iamgio-quarkdown-project-files-images-tbanner-light.svg\" " +
+                    "alt=\"Banner\" />" +
+                    "</p>" +
+                    "</div>",
+                it,
+            )
+
+            assertEquals(2, mediaStorage.all.size)
+        }
+
+        execute(
+            """
+            ![Banner](https://raw.githubusercontent.com/iamgio/quarkdown/project-files/images/tbanner-light.svg)  
+            ![Quarkdown](img/icon.png)
+            """.trimIndent(),
+            options = MutableContextOptions(enableRemoteMediaStorage = false),
+            enableMediaStorage = true,
+        ) {
+            assertEquals(
+                "<p>" +
+                    "<img src=\"https://raw.githubusercontent.com/iamgio/quarkdown/project-files/images/tbanner-light.svg\" " +
+                    "alt=\"Banner\" /><br /><img src=\"media/",
+                it.toString().substringBefore("icon-"),
+            )
+
+            assertEquals(1, mediaStorage.all.size)
+        }
+    }
 }
