5cfcd6d95c0ab6cb133d89bd7d5f8c61b02d1634
Giorgio Garofalo <giogar2705@gmail.com>
2024-09-11 17:49:02 +0200

    Set text replacement type to char instead of string

--- core/src/main/kotlin/eu/iamgio/quarkdown/ast/quarkdown/inline/TextSymbol.kt
+++ core/src/main/kotlin/eu/iamgio/quarkdown/ast/quarkdown/inline/TextSymbol.kt
@@ -6,9 +6,15 @@ import eu.iamgio.quarkdown.visitor.node.NodeVisitor
 /**
  * A text-based symbol, such as `©`, `…`, `≥`.
  * This is usually the result of a combination of multiple characters (e.g. `(C)` -> `©`).
- * @param text processed symbol (e.g. `©`)
+ * @param symbol processed symbol (e.g. `©`)
  * @see eu.iamgio.quarkdown.lexer.patterns.TextSymbolReplacement
  */
-data class TextSymbol(override val text: String) : PlainTextNode {
+data class TextSymbol(val symbol: Char) : PlainTextNode {
+    /**
+     * @return [symbol] as a string
+     */
+    override val text: String
+        get() = symbol.toString()
+
     override fun <T> accept(visitor: NodeVisitor<T>): T = visitor.visit(this)
 }
