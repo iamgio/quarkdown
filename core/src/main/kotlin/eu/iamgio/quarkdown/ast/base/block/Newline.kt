786f1044ce0d49d3099c9cb0da92d9529638a615
Giorgio Garofalo <giogar2705@gmail.com>
2024-09-18 16:39:38 +0200

    Make Newline be an empty text node

--- core/src/main/kotlin/eu/iamgio/quarkdown/ast/base/block/Newline.kt
+++ core/src/main/kotlin/eu/iamgio/quarkdown/ast/base/block/Newline.kt
@@ -1,11 +1,14 @@
 package eu.iamgio.quarkdown.ast.base.block
 
 import eu.iamgio.quarkdown.ast.Node
+import eu.iamgio.quarkdown.ast.base.TextNode
 import eu.iamgio.quarkdown.visitor.node.NodeVisitor
 
 /**
  * A blank line.
  */
-data object Newline : Node {
+data object Newline : TextNode {
+    override val text: List<Node> = emptyList()
+
     override fun <T> accept(visitor: NodeVisitor<T>) = visitor.visit(this)
 }
