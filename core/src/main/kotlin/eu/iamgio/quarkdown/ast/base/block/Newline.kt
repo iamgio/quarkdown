3c17bc71f3e0a56d89207781adcbbd465e818ae9
Giorgio Garofalo <giogar2705@gmail.com>
2024-09-18 16:46:35 +0200

    Revert: Make Newline be an empty text node

--- core/src/main/kotlin/eu/iamgio/quarkdown/ast/base/block/Newline.kt
+++ core/src/main/kotlin/eu/iamgio/quarkdown/ast/base/block/Newline.kt
@@ -1,14 +1,11 @@
 package eu.iamgio.quarkdown.ast.base.block
 
 import eu.iamgio.quarkdown.ast.Node
-import eu.iamgio.quarkdown.ast.base.TextNode
 import eu.iamgio.quarkdown.visitor.node.NodeVisitor
 
 /**
  * A blank line.
  */
-data object Newline : TextNode {
-    override val text: List<Node> = emptyList()
-
+data object Newline : Node {
     override fun <T> accept(visitor: NodeVisitor<T>) = visitor.visit(this)
 }
