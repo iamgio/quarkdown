54a2896b26dce57af37859e1e94e213a4b708ee2
Giorgio Garofalo <giogar2705@gmail.com>
2024-11-24 16:17:29 +0100

    Export .qmd libs to distzip

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,15 @@ tasks.test {
 
 tasks.distZip {
     archiveVersion.set("")
+
+    // The module 'libs' contains .qmd library files that are saved in the lib/qd directory of the distribution zip.
+    val librariesModule = project(":libs")
+
+    into("${archiveBaseName.get()}/lib/qd") {
+        from(librariesModule.file("src/main/resources")) {
+            include("*.qmd")
+        }
+    }
 }
 
 tasks.wrapper {
---
+++ libs/build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "eu.iamgio.quarkdown"
+version = "1.0-SNAPSHOT"
