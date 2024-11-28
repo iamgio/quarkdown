aa9dd9b62f452a704213b1d1238735a85a422922
Giorgio Garofalo <giogar2705@gmail.com>
2024-11-28 18:32:23 +0100

    Add string utilities: .string, .concatenate .isempty, .isnotempty

--- stdlib/src/main/kotlin/eu/iamgio/quarkdown/stdlib/String.kt
+++ stdlib/src/main/kotlin/eu/iamgio/quarkdown/stdlib/String.kt
@@ -1,8 +1,11 @@
 package eu.iamgio.quarkdown.stdlib
 
+import eu.iamgio.quarkdown.function.reflect.annotation.Name
+import eu.iamgio.quarkdown.function.value.StringValue
 import eu.iamgio.quarkdown.function.value.wrappedAsValue
 import eu.iamgio.quarkdown.util.StringCase
 import eu.iamgio.quarkdown.util.case
+import eu.iamgio.quarkdown.util.trimDelimiters
 
 /**
  * `String` stdlib module exporter.
@@ -10,10 +13,47 @@ import eu.iamgio.quarkdown.util.case
  */
 val String: Module =
     setOf(
+        ::string,
+        ::concatenate,
         ::uppercase,
         ::lowercase,
         ::capitalize,
+        ::isEmpty,
+        ::isNotEmpty,
     )
+
+/**
+ * Creates a string.
+ * If [value] is delimited by `"` characters, they are removed
+ * and the wrapped string is not trimmed, as opposed to what usually happens
+ * through Quarkdown's parser.
+ * Example: `"  Hello, World!  "` -> `  Hello, World!  `
+ * @param value string to wrap
+ * @return a new string value
+ */
+fun string(value: String) =
+    when {
+        value.firstOrNull() == '\"' && value.lastOrNull() == '\"' -> value.trimDelimiters()
+        else -> value
+    }.wrappedAsValue()
+
+/**
+ * Concatenates two strings if a condition is met.
+ * Example: `Hello, ` and `World!` -> `Hello, World!`
+ * @param a first string
+ * @param b second string
+ * @param condition if true, concatenates `a` and `b`
+ * @return a new string that is the concatenation of `a` and `b` if `condition` is true, `a` otherwise
+ */
+fun concatenate(
+    a: String,
+    @Name("with") b: String,
+    @Name("if") condition: Boolean = true,
+): StringValue =
+    when {
+        condition -> a + b
+        else -> a
+    }.wrappedAsValue()
 
 /**
  * Converts a string to uppercase.
@@ -38,3 +78,20 @@ fun lowercase(string: String) = string.case(StringCase.Lower).wrappedAsValue()
  * @return a new string with the first character capitalized
  */
 fun capitalize(string: String) = string.case(StringCase.Capitalize).wrappedAsValue()
+
+/**
+ * Checks if a string is empty.
+ * @param string string to check
+ * @return `true` if the string is empty, `false` otherwise
+ */
+@Name("isempty")
+fun isEmpty(string: String) = string.isEmpty().wrappedAsValue()
+
+/**
+ * Checks if a string is not empty.
+ * @param string string to check
+ * @return `true` if the string is not empty, `false` otherwise
+ * @see isEmpty
+ */
+@Name("isnotempty")
+fun isNotEmpty(string: String) = string.isNotEmpty().wrappedAsValue()
