1ea4b11fb7f3106297e44f4bd51b5b3c47716693
Giorgio Garofalo <giogar2705@gmail.com>
2024-11-29 01:09:18 +0100

    Remove leftover todo

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     implementation(kotlin("reflect"))
     testImplementation(kotlin("test"))
     testImplementation("org.assertj:assertj-core:3.26.3")
-    implementation("com.github.h0tk3y.betterParse:better-parse:0.4.4") // todo apply tests from parser-test toy project
+    implementation("com.github.h0tk3y.betterParse:better-parse:0.4.4")
     implementation("org.apache.logging.log4j:log4j-core:2.23.1")
     implementation("org.apache.commons:commons-text:1.11.0")
     implementation("com.github.ajalt.colormath:colormath:3.6.0")
