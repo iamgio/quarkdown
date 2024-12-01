afe2ae8e8fcd40fa1977d0d506506d0b0c0e446e
Giorgio Garofalo <giogar2705@gmail.com>
2024-12-01 22:06:11 +0100

    Reload on websocket, keep scroll position

--- core/src/main/resources/render/script/websockets.js
+++ core/src/main/resources/render/script/websockets.js
@@ -1,5 +1,56 @@
 // Component that enables communication with a server through WebSockets.
 
 function startWebSockets(serverUrl) {
-    console.log('yay! ' + serverUrl)
+    startReloadWebSocket(serverUrl);
 }
+
+function createWebSocket(serverUrl, endpoint) {
+    return new WebSocket(`ws://${serverUrl}/${endpoint}`);
+}
+
+// WebSocket that reloads the page when a message is received.
+function startReloadWebSocket(serverUrl) {
+    const socket = createWebSocket(serverUrl, 'reload')
+
+    socket.addEventListener('open', () => {
+        console.log('Connected to server ' + socket.url);
+    });
+
+    socket.addEventListener('message', (event) => {
+        location.reload();
+    });
+
+    socket.addEventListener("close", (event) => {
+        console.log(`WebSocket closed: Code=${event.code}, Reason=${event.reason}`);
+    });
+
+    socket.addEventListener("error", (error) => {
+        console.error("WebSocket error:", error);
+    });
+}
+
+// Saves scroll position.
+function saveScrollPosition() {
+    history.scrollRestoration = "manual";
+    const scrollPosition = {
+        top: window.scrollY,
+        left: window.scrollX
+    };
+    localStorage.setItem("scrollPosition", JSON.stringify(scrollPosition));
+}
+
+// Restores scroll position.
+function restoreScrollPosition() {
+    const savedPosition = localStorage.getItem("scrollPosition");
+    if (savedPosition) {
+        const {top, left} = JSON.parse(savedPosition);
+        window.scrollTo({top, left, behavior: "auto"});
+        localStorage.removeItem("scrollPosition"); // Clean up.
+    }
+}
+
+// Save scroll position before reload.
+window.addEventListener("beforeunload", saveScrollPosition);
+
+// Restore after reload to keep the scroll position.
+executionQueue.push(restoreScrollPosition)
