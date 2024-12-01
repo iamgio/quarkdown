22f575e2516d1fb85a1519fb6b897446d89168a0
Giorgio Garofalo <giogar2705@gmail.com>
2024-12-01 22:43:11 +0100

    Improve websockets.js socket creation

--- core/src/main/resources/render/script/websockets.js
+++ core/src/main/resources/render/script/websockets.js
@@ -4,29 +4,29 @@ function startWebSockets(serverUrl) {
     startReloadWebSocket(serverUrl);
 }
 
-function createWebSocket(serverUrl, endpoint) {
-    return new WebSocket(`ws://${serverUrl}/${endpoint}`);
-}
-
-// WebSocket that reloads the page when a message is received.
-function startReloadWebSocket(serverUrl) {
-    const socket = createWebSocket(serverUrl, 'reload')
+function createWebSocket(serverUrl, endpoint, onMessage) {
+    const socket = new WebSocket(`ws://${serverUrl}/${endpoint}`);
 
     socket.addEventListener('open', () => {
         console.log('Connected to server ' + socket.url);
     });
 
-    socket.addEventListener('message', (event) => {
-        location.reload();
-    });
+    socket.addEventListener("message", onMessage);
 
-    socket.addEventListener("close", (event) => {
+    socket.addEventListener('close', (event) => {
         console.log(`WebSocket closed: Code=${event.code}, Reason=${event.reason}`);
     });
 
     socket.addEventListener("error", (error) => {
         console.error("WebSocket error:", error);
     });
+
+    return socket;
+}
+
+// WebSocket that reloads the page when a message is received.
+function startReloadWebSocket(serverUrl) {
+    createWebSocket(serverUrl, 'reload', () => location.reload());
 }
 
 // Saves scroll position.
