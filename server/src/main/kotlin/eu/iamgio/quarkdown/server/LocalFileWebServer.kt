3ad25bd3fffb468f30339b22ba3359e5cdf397ea
Giorgio Garofalo <giogar2705@gmail.com>
2024-12-01 21:10:37 +0100

    Add WebSocket broadcast to clients

--- server/src/main/kotlin/eu/iamgio/quarkdown/server/LocalFileWebServer.kt
+++ server/src/main/kotlin/eu/iamgio/quarkdown/server/LocalFileWebServer.kt
@@ -1,14 +1,19 @@
 package eu.iamgio.quarkdown.server
 
 import io.ktor.server.application.install
+import io.ktor.server.application.log
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.http.content.staticFiles
 import io.ktor.server.netty.Netty
 import io.ktor.server.routing.routing
+import io.ktor.server.websocket.DefaultWebSocketServerSession
 import io.ktor.server.websocket.WebSockets
 import io.ktor.server.websocket.pingPeriod
 import io.ktor.server.websocket.timeout
 import io.ktor.server.websocket.webSocket
+import io.ktor.websocket.Frame
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.io.File
 import kotlin.time.Duration.Companion.seconds
 
@@ -24,6 +29,18 @@ class LocalFileWebServer(private val targetFile: File) : Server {
     override fun start(port: Int) {
         if (!targetFile.exists()) throw IllegalArgumentException("Cannot start web server from non-existing file: $targetFile")
 
+        // Connections to the server.
+        val sessions = mutableListOf<DefaultWebSocketServerSession>()
+        // Mutex to prevent concurrent modification of [sessions].
+        val mutex = Mutex()
+
+        // Sends a frame to all connected clients.
+        suspend fun sendBroadcast(frame: Frame) {
+            mutex.withLock {
+                sessions.forEach { it.send(frame) }
+            }
+        }
+
         embeddedServer(Netty, port) {
             install(WebSockets) {
                 pingPeriod = 5.seconds
@@ -32,9 +49,19 @@ class LocalFileWebServer(private val targetFile: File) : Server {
             }
             routing {
                 webSocket("/reload") {
-                    println("Reload requested")
+                    // Reload flow:
+                    // 1. The Quarkdown CLI sends a WebSocket to the server after the new files are generated.
+                    // 2. The server forwards the message to all connected clients, e.g. the browser (see websockets.js).
+
+                    log.info("Reload requested")
+                    mutex.withLock { sessions += this }
+
+                    for (frame in incoming) {
+                        sendBroadcast(frame)
+                    }
                 }
 
+                // Serve the target file.
                 staticFiles("/", targetFile)
             }
         }.start(wait = true)
