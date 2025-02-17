a9f680aa2aa20821e2bddf8cf84e00089dafa8d7
Giorgio Garofalo <giogar2705@gmail.com>
2025-02-17 21:50:02 +0100

    Fix inline collapsible not processing HTML

--- core/src/main/resources/render/script/script.js
+++ core/src/main/resources/render/script/script.js
@@ -42,18 +42,25 @@ executionQueue.push(() => {
     // Add click event listener to the collapsible spans.
     const collapsibles = document.querySelectorAll('.inline-collapse');
     collapsibles.forEach((span) => {
-        span.addEventListener('click', toggleCollapse);
+        span.addEventListener('click', () => toggleCollapse(span));
     });
 });
 
 // Toggles the collapsed/expanded state of inline collapsibles.
-function toggleCollapse(event) {
-    const span = event.target;
+function toggleCollapse(span) {
     const fullText = span.dataset.fullText;
     const collapsedText = span.dataset.collapsedText;
     const collapsed = span.dataset.collapsed === 'true';
 
     // Toggle between the full and collapsed text.
-    span.textContent = collapsed ? fullText : collapsedText;
+    const content = collapsed ? fullText : collapsedText;
+
     span.dataset.collapsed = (!collapsed).toString();
+
+    const isUserDefined = span.closest('.error') === null;
+    if (isUserDefined) {
+        span.innerHTML = content;
+    } else {
+        span.textContent = content;
+    }
 }
