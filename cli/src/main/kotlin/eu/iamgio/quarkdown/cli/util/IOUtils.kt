1e85b09be04e92bd80bf177c78d6e8440a4c4eab
Giorgio Garofalo <giogar2705@gmail.com>
2025-04-01 20:00:42 +0200

    Fix `--clean` uninstalling Puppeteer

--- cli/src/main/kotlin/eu/iamgio/quarkdown/cli/util/IOUtils.kt
+++ cli/src/main/kotlin/eu/iamgio/quarkdown/cli/util/IOUtils.kt
@@ -3,19 +3,28 @@ package eu.iamgio.quarkdown.cli.util
 import eu.iamgio.quarkdown.pipeline.output.FileResourceExporter
 import eu.iamgio.quarkdown.pipeline.output.OutputResource
 import java.io.File
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.deleteRecursively
 
 /**
  * The executable JAR file location, if available.
  */
 val thisExecutableFile: File?
-    get() = object {}.javaClass.protectionDomain?.codeSource?.location?.toURI()?.let(::File)
+    get() =
+        object {}
+            .javaClass.protectionDomain
+            ?.codeSource
+            ?.location
+            ?.toURI()
+            ?.let(::File)
 
 /**
  * Cleans [this] directory by deleting all files and directories inside it.
  * Does nothing if the directory is empty or if the file does not exist or is not a directory.
  */
+@OptIn(ExperimentalPathApi::class)
 fun File.cleanDirectory() {
-    listFiles()?.forEach { it.deleteRecursively() }
+    listFiles()?.forEach { it.toPath().deleteRecursively() }
 }
 
 /**
@@ -23,6 +32,4 @@ fun File.cleanDirectory() {
  * @see FileResourceExporter
  * @return the saved file
  */
-fun OutputResource.saveTo(directory: File): File {
-    return accept(FileResourceExporter(location = directory))
-}
+fun OutputResource.saveTo(directory: File): File = accept(FileResourceExporter(location = directory))
