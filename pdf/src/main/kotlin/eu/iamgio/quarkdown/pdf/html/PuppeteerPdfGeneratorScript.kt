306794db0e002c53f2db4e9c9e0f72889f2f7f09
Giorgio Garofalo <giogar2705@gmail.com>
2025-04-08 22:32:18 +0200

    Add NodeNpmHelper

--- pdf/src/main/kotlin/eu/iamgio/quarkdown/pdf/html/PuppeteerPdfGeneratorScript.kt
+++ pdf/src/main/kotlin/eu/iamgio/quarkdown/pdf/html/PuppeteerPdfGeneratorScript.kt
@@ -1,13 +1,12 @@
 package eu.iamgio.quarkdown.pdf.html
 
 import eu.iamgio.quarkdown.interaction.executable.NodeJsWrapper
+import eu.iamgio.quarkdown.interaction.executable.NodeNpmHelper
 import eu.iamgio.quarkdown.interaction.executable.NpmWrapper
 import eu.iamgio.quarkdown.log.Log
 import eu.iamgio.quarkdown.server.LocalFileWebServer
 import eu.iamgio.quarkdown.server.withScanner
 import java.io.File
-import kotlin.io.path.ExperimentalPathApi
-import kotlin.io.path.deleteRecursively
 
 /**
  * The starting port to attempt to start the server on.
@@ -35,44 +34,26 @@ class PuppeteerPdfGeneratorScript(
     /**
      * Launches Puppeteer to convert the webpage from [directory] into a PDF saved at [out].
      */
-    fun launch() {
-        checkNode()
-        checkNpm()
-        linkPuppeteer()
+    fun launch() =
+        NodeNpmHelper(directory, node, npm).launch(PuppeteerNodeModule) {
+            launchServer()
+        }
 
-        LocalFileWebServer(directory).withScanner().attemptStartUntilPortAvailable(STARTING_SERVER_PORT) { server, port ->
-            this.port = port
-            Log.info("PDF server is ready on port $port.")
-            try {
-                runScript()
-                Log.info("PDF generated at $out")
-            } catch (e: Exception) {
-                throw e
-            } finally {
-                server.stop()
-                cleanup()
+    private fun launchServer() {
+        LocalFileWebServer(directory)
+            .withScanner()
+            .attemptStartUntilPortAvailable(STARTING_SERVER_PORT) { server, port ->
+                this.port = port
+                Log.info("PDF server is ready on port $port.")
+                try {
+                    runScript()
+                    Log.info("PDF generated at $out")
+                } catch (e: Exception) {
+                    throw e
+                } finally {
+                    server.stop()
+                }
             }
-        }
-    }
-
-    private fun checkNode() {
-        if (!node.isValid) {
-            throw IllegalStateException("Node.js executable cannot be found at '${node.path}'")
-        }
-    }
-
-    private fun checkNpm() {
-        if (!npm.isValid) {
-            throw IllegalStateException("NPM executable cannot be found at '${npm.path}'")
-        }
-    }
-
-    private fun linkPuppeteer() {
-        if (!npm.isInstalled(PuppeteerNodeModule)) {
-            Log.info("Puppeteer is not installed. Installing...")
-            npm.install(PuppeteerNodeModule)
-        }
-        npm.link(node, PuppeteerNodeModule)
     }
 
     private fun runScript() {
@@ -82,15 +63,5 @@ class PuppeteerPdfGeneratorScript(
         val url = "http://localhost:$port/?print-pdf"
 
         node.eval(script.reader(), out.absolutePath, url, noSandbox.toString())
-    }
-
-    @OptIn(ExperimentalPathApi::class)
-    private fun cleanup() {
-        // Path#deleteRecursively does not follow symlinks, while File#deleteRecursively does.
-        // The symlinks contained in the node_modules directory point to the global packages,
-        // and should not be deleted.
-        sequenceOf("package.json", "package-lock.json", "node_modules")
-            .map { File(directory, it).toPath() }
-            .forEach { it.deleteRecursively() }
     }
 }
