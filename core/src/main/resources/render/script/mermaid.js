b008df715ba1e262ef4ba3c9a78d566ab62dfd64
Giorgio Garofalo <giogar2705@gmail.com>
2025-04-11 13:23:56 +0200

    Realign misaligned Mermaid elements

--- core/src/main/resources/render/script/mermaid.js
+++ core/src/main/resources/render/script/mermaid.js
@@ -7,6 +7,14 @@ function initMermaid(mermaid) {
     // Render Mermaid diagrams.
     executionQueue.push(async () => {
         await mermaid.run();
+        realignMermaidContents();
         notifyTaskFinished();
+    });
+}
+
+// Only after rendering the diagrams, center some misaligned elements.
+function realignMermaidContents() {
+    document.querySelectorAll('.mermaid foreignObject').forEach((obj) => {
+        obj.style.display = 'grid';
     });
 }
