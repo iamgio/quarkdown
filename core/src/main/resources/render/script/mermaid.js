226f448497d3ecca8a5947951884836d00d5791a
Giorgio Garofalo <giogar2705@gmail.com>
2025-04-12 17:32:53 +0200

    Session-cache Mermaid diagrams

--- core/src/main/resources/render/script/mermaid.js
+++ core/src/main/resources/render/script/mermaid.js
@@ -3,11 +3,33 @@ function initMermaid(mermaid) {
         startOnLoad: false,
     });
 
-    // Render Mermaid diagrams.
-    preRenderingExecutionQueue.push(async () => {
-        await mermaid.run();
+    // Render Mermaid diagrams or load them from cache.
+    postRenderingExecutionQueue.push(async () => {
+        for (const element of document.querySelectorAll('.mermaid')) {
+            await loadFromCacheOrRender(mermaid, element);
+        }
         realignMermaidContents();
     });
+}
+
+async function loadFromCacheOrRender(mermaid, element) {
+    const code = element.textContent.trim();
+    const id = 'mermaid-' + hashCode(code);
+    const cachedSvg = sessionStorage.getItem(id);
+    element.dataset.processed = 'true';
+
+    if (cachedSvg) {
+        console.debug('Using cached SVG for diagram:', id);
+        element.innerHTML = cachedSvg;
+        return;
+    }
+
+    console.debug('Rendering diagram:', id);
+
+    const diagram = await mermaid.render(id, code, element);
+    const svg = diagram.svg;
+    element.innerHTML = svg;
+    sessionStorage.setItem(id, svg);
 }
 
 // Only after rendering the diagrams, center some misaligned elements.
--- core/src/main/resources/render/script/script.js
+++ core/src/main/resources/render/script/script.js
@@ -175,3 +175,13 @@ function applyRemainingHeightProperties() {
 }
 
 postRenderingExecutionQueue.push(applyRemainingHeightProperties);
+
+// General hash utility.
+function hashCode(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = ((hash << 5) - hash) + str.charCodeAt(i);
+        hash |= 0;
+    }
+    return hash.toString();
+}
