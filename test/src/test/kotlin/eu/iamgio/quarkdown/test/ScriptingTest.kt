b328c22709c0b53f7fd655b1bd41fe320cab6fbf
Giorgio Garofalo <giogar2705@gmail.com>
2025-04-25 01:03:50 +0200

    Add chart plotting from CSV test

--- test/src/test/kotlin/eu/iamgio/quarkdown/test/ScriptingTest.kt
+++ test/src/test/kotlin/eu/iamgio/quarkdown/test/ScriptingTest.kt
@@ -785,4 +785,35 @@ class ScriptingTest {
             )
         }
     }
+
+    @Test
+    fun `chart from csv`() {
+        execute(
+            """
+            .let {.csv {csv/sales.csv}}
+                data:
+                .var {columns}
+                    .tablecolumns
+                        .data
+            
+                .xychart xtags:{.columns::first} y:{Sales} bars:{yes}
+                    .columns::second
+                    .columns::third
+            """.trimIndent(),
+        ) {
+            assertEquals(
+                """
+                <figure><pre class="mermaid fill-height">xychart-beta
+                    x-axis [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025]
+                    y-axis "Sales"
+                    bar [230.0, 190.0, 180.0, 175.0, 200.0, 250.0, 290.0, 350.0, 470.0]
+                    line [230.0, 190.0, 180.0, 175.0, 200.0, 250.0, 290.0, 350.0, 470.0]
+                    bar [85.0, 100.0, 135.0, 180.0, 240.0, 320.0, 430.0, 580.0, 800.0, 0.0]
+                    line [85.0, 100.0, 135.0, 180.0, 240.0, 320.0, 430.0, 580.0, 800.0, 0.0]
+                </pre></figure>
+                """.trimIndent(),
+                it.toString().replace("\t", "    "),
+            )
+        }
+    }
 }
