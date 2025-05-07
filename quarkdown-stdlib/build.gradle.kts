82f6b06e2167ba919b69432fa8b9c56214f822f0
Giorgio Garofalo <giogar2705@gmail.com>
2025-05-07 17:47:25 +0200

    Add styling to quarkdoc

--- quarkdown-stdlib/build.gradle.kts
+++ quarkdown-stdlib/build.gradle.kts
@@ -1,3 +1,7 @@
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.base.DokkaBaseConfiguration
+import java.time.Year
+
 plugins {
     kotlin("jvm")
 }
@@ -6,5 +10,24 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     implementation(project(":quarkdown-core"))
     implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.10.0")
+    implementation("org.jetbrains.dokka:dokka-base:2.0.0")
     dokkaPlugin(project(":quarkdown-quarkdoc"))
+}
+
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.dokka:dokka-base:2.0.0")
+    }
+}
+
+// The following might become a Gradle plugin in the future.
+tasks.dokkaHtml.configure {
+    fun asset(path: String): File = project(":quarkdown-quarkdoc").projectDir.resolve("src/main/resources/$path")
+
+    pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+        val year = Year.now().value
+        footerMessage = "&copy; $year Quarkdown"
+        customAssets = customAssets + asset("assets/images").listFiles()!!
+        customStyleSheets = customStyleSheets + listOf(asset("styles/stylesheet.css"))
+    }
 }
