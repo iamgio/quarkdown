5b5bd590467778ed19d7a139f972fb8f582fbbbc
iamgio <iamgio@users.noreply.github.com>
2025-05-11 20:55:47 +0000

    deploy: 054449b3b31a3006183d659d8423699845d0c509

---
+++ docs/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":quarkdown-cli/main":[],":quarkdown-stdlib/main":[],":quarkdown-interaction/main":[],":quarkdown-core/main":[],":quarkdown-pdf/main":[],":quarkdown-quarkdoc/main":[],":quarkdown-server/main":[]}'
