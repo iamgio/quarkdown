59b0b573f7b9286533048139ef134910be46f427
Giorgio Garofalo <giogar2705@gmail.com>
2025-05-18 22:03:35 +0200

    Add `.includeall`

--- quarkdown-stdlib/src/main/kotlin/com/quarkdown/stdlib/Ecosystem.kt
+++ quarkdown-stdlib/src/main/kotlin/com/quarkdown/stdlib/Ecosystem.kt
@@ -5,10 +5,15 @@ import com.quarkdown.core.context.MutableContext
 import com.quarkdown.core.function.library.loader.Module
 import com.quarkdown.core.function.library.loader.moduleOf
 import com.quarkdown.core.function.reflect.annotation.Injected
+import com.quarkdown.core.function.reflect.annotation.Name
+import com.quarkdown.core.function.value.GeneralCollectionValue
+import com.quarkdown.core.function.value.IterableValue
 import com.quarkdown.core.function.value.NodeValue
 import com.quarkdown.core.function.value.OutputValue
+import com.quarkdown.core.function.value.Value
 import com.quarkdown.core.function.value.VoidValue
 import com.quarkdown.core.function.value.factory.ValueFactory
+import com.quarkdown.stdlib.internal.asString
 import java.io.Reader
 
 /**
@@ -18,6 +23,7 @@ import java.io.Reader
 val Ecosystem: Module =
     moduleOf(
         ::include,
+        ::includeAll,
     )
 
 /**
@@ -47,7 +53,7 @@ internal fun includeResource(
  * The context of the main file is shared to the sub-file and vice versa, allowing for sharing of variables, functions and other declarations.
  * @param path either a path (relative or absolute with extension) to the file to include, or the name of a loadable library
  * @return the content of the file as a node if a file is included, or nothing if a library is loaded
- * @throws IllegalArgumentException if the loaded Quarkdown source cannot be evaluated or if it cannot be evaluated into a suitable output value
+ * @throws IllegalArgumentException if the loaded Quarkdown source cannot be evaluated
  * @wiki Including other Quarkdown files
  */
 fun include(
@@ -61,3 +67,20 @@ fun include(
     val file = file(context, path)
     return includeResource(context, file.bufferedReader())
 }
+
+/**
+ * Performs a bulk include of the given paths via [include].
+ * @param paths paths to the files or library names to include
+ * @return a [GeneralCollectionValue] containing the output of the included files
+ * @throws IllegalArgumentException if any of the loaded sources cannot be evaluated
+ * @see include for information about file inclusion
+ * @wiki Including other Quarkdown files
+ */
+@Name("includeall")
+fun includeAll(
+    @Injected context: MutableContext,
+    paths: Iterable<Value<*>>,
+): IterableValue<OutputValue<*>> =
+    paths
+        .map { include(context, it.asString()) }
+        .let(::GeneralCollectionValue)
--- quarkdown-stdlib/src/main/kotlin/com/quarkdown/stdlib/internal/Types.kt
+++ quarkdown-stdlib/src/main/kotlin/com/quarkdown/stdlib/internal/Types.kt
@@ -5,10 +5,19 @@ import com.quarkdown.core.function.value.Value
 // Types utilities for the stdlib.
 
 /**
+ * Converts [Value] to a [String].
+ */
+internal fun Value<*>.asString(): String =
+    when (val value = unwrappedValue) {
+        is Value<*> -> value.asString()
+        else -> value.toString()
+    }
+
+/**
  * Converts [Value] to a [Double].
  * @return the value as a double, or 0 if the value is not numeric
  */
-fun Value<*>.asDouble(): Double =
+internal fun Value<*>.asDouble(): Double =
     when (val value = unwrappedValue) {
         is Number -> value.toDouble()
         else -> value.toString().toDoubleOrNull()
