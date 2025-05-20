20b3ed8d3f22b7331c71de0cfc40ecf181a890aa
Giorgio Garofalo <giogar2705@gmail.com>
2025-05-20 22:05:28 +0200

    Rename CliOptions#generatePdf to exportPdf

--- quarkdown-cli/src/main/kotlin/com/quarkdown/cli/exec/CompileCommand.kt
+++ quarkdown-cli/src/main/kotlin/com/quarkdown/cli/exec/CompileCommand.kt
@@ -49,7 +49,7 @@ class CompileCommand : ExecuteCommand("c") {
         original.copy(
             source = source,
             noPdfSandbox = noPdfSandbox,
-            generatePdf = exportPdf,
+            exportPdf = exportPdf,
         )
 
     override fun createExecutionStrategy(cliOptions: CliOptions) = FileExecutionStrategy(source)
