99b0558c7889421531fdf75dae9cba79ef4b89f4
Giorgio Garofalo <giogar2705@gmail.com>
2025-05-20 16:58:30 +0200

    Update RendererFactory docs

--- quarkdown-core/src/main/kotlin/com/quarkdown/core/flavor/RendererFactory.kt
+++ quarkdown-core/src/main/kotlin/com/quarkdown/core/flavor/RendererFactory.kt
@@ -5,11 +5,22 @@ import com.quarkdown.core.flavor.quarkdown.QuarkdownRendererFactory
 
 /**
  * Provider of rendering strategies.
+ * This factory is populated by extensions provided by external modules, such as `quarkdown-html`.
+ *
+ * See [com.quarkdown.rendering.html.extension.html] for an example of a renderer extension.
  */
 interface RendererFactory {
+    /**
+     * Accepts a visitor to this renderer factory.
+     * @param visitor the visitor to accept
+     */
     fun <T> accept(visitor: RendererFactoryVisitor<T>): T
 }
 
+/**
+ * Visitor for renderer factories of different [MarkdownFlavor].
+ * @param T the type of the result of the visit operation
+ */
 interface RendererFactoryVisitor<T> {
     fun visit(factory: BaseMarkdownRendererFactory): T
 
