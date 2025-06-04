4077560908f4c60a3c613855f122304c01f983d1
Giorgio Garofalo <giogar2705@gmail.com>
2025-06-05 00:06:40 +0200

    quarkdoc: refactor AdditionalParameterPropertiesTransformer EnumParameterEntryListerTransformer via common QuarkdocParameterDocumentationTransformer

--- quarkdown-quarkdoc/src/main/kotlin/com/quarkdown/quarkdoc/dokka/transformers/enumeration/EnumParameterEntryListerTransformer.kt
+++ quarkdown-quarkdoc/src/main/kotlin/com/quarkdown/quarkdoc/dokka/transformers/enumeration/EnumParameterEntryListerTransformer.kt
@@ -1,20 +1,15 @@
 package com.quarkdown.quarkdoc.dokka.transformers.enumeration
 
 import com.quarkdown.core.function.toQuarkdownNamingFormat
-import com.quarkdown.core.util.filterNotNullEntries
-import com.quarkdown.quarkdoc.dokka.kdoc.mapDocumentation
-import com.quarkdown.quarkdoc.dokka.transformers.QuarkdocDocumentableReplacerTransformer
+import com.quarkdown.quarkdoc.dokka.transformers.QuarkdocParameterDocumentationTransformer
 import com.quarkdown.quarkdoc.dokka.transformers.enumeration.adapters.QuarkdocEnumAdapters
-import com.quarkdown.quarkdoc.dokka.util.tryCopy
 import org.jetbrains.dokka.base.signatures.KotlinSignatureUtils.driOrNull
-import org.jetbrains.dokka.model.DFunction
 import org.jetbrains.dokka.model.DParameter
 import org.jetbrains.dokka.model.doc.CodeInline
 import org.jetbrains.dokka.model.doc.DocTag
 import org.jetbrains.dokka.model.doc.DocumentationLink
 import org.jetbrains.dokka.model.doc.H4
 import org.jetbrains.dokka.model.doc.Li
-import org.jetbrains.dokka.model.doc.Param
 import org.jetbrains.dokka.model.doc.Text
 import org.jetbrains.dokka.model.doc.Ul
 import org.jetbrains.dokka.plugability.DokkaContext
@@ -25,22 +20,13 @@ import org.jetbrains.dokka.plugability.DokkaContext
  */
 class EnumParameterEntryListerTransformer(
     context: DokkaContext,
-) : QuarkdocDocumentableReplacerTransformer(context) {
+) : QuarkdocParameterDocumentationTransformer<QuarkdocEnum>(context) {
     /**
-     * @return the parameters, among [parameters], that expect an enum value, associated with their enum declaration.
+     * @return the enum type of the parameter, if it is an enum
      */
-    private fun associateEnumParameters(parameters: List<DParameter>): Map<String, QuarkdocEnum> =
-        parameters
-            .asSequence()
-            .map { it.name to it.type.driOrNull?.let(QuarkdocEnumAdapters::fromDRI) }
-            .filterNotNullEntries()
-            .toMap()
+    override fun extractValue(parameter: DParameter): QuarkdocEnum? = parameter.type.driOrNull?.let(QuarkdocEnumAdapters::fromDRI)
 
-    /**
-     * @return the documentation content to add to the parameter documentation,
-     * which lists the enum entries of the given [enum].
-     */
-    private fun createNewDocumentationContent(enum: QuarkdocEnum): List<DocTag> =
+    override fun createNewDocumentation(value: QuarkdocEnum): List<DocTag> =
         listOf(
             H4(
                 listOf(
@@ -48,7 +34,7 @@ class EnumParameterEntryListerTransformer(
                 ),
             ),
             Ul(
-                enum.entries.map { entry ->
+                value.entries.map { entry ->
                     Li(
                         listOf(
                             DocumentationLink(
@@ -67,24 +53,8 @@ class EnumParameterEntryListerTransformer(
             ),
         )
 
-    override fun transformFunction(function: DFunction): AnyWithChanges<DFunction> {
-        val enumParameters: Map<String, QuarkdocEnum> =
-            associateEnumParameters(function.parameters)
-                .takeIf { it.isNotEmpty() }
-                ?: return function.unchanged()
-
-        // Updates the documentation of the parameters to include the enum entries.
-        val documentation =
-            mapDocumentation(function.documentation) {
-                register(Param::class) { param ->
-                    val enum = enumParameters[param.name] ?: return@register param
-                    val root = param.root
-                    param.copy(
-                        root = root.tryCopy(newChildren = root.children + createNewDocumentationContent(enum)),
-                    )
-                }
-            }
-
-        return function.copy(documentation = documentation).changed()
-    }
+    override fun mergeDocumentationContent(
+        old: List<DocTag>,
+        new: List<DocTag>,
+    ) = old + new
 }
--- quarkdown-quarkdoc/src/main/kotlin/com/quarkdown/quarkdoc/dokka/transformers/optional/AdditionalParameterPropertiesTransformer.kt
+++ quarkdown-quarkdoc/src/main/kotlin/com/quarkdown/quarkdoc/dokka/transformers/optional/AdditionalParameterPropertiesTransformer.kt
@@ -1,17 +1,12 @@
 package com.quarkdown.quarkdoc.dokka.transformers.optional
 
 import com.quarkdown.core.function.value.factory.ValueFactory.enum
-import com.quarkdown.core.util.filterNotNullEntries
-import com.quarkdown.quarkdoc.dokka.kdoc.mapDocumentation
-import com.quarkdown.quarkdoc.dokka.transformers.QuarkdocDocumentableReplacerTransformer
-import com.quarkdown.quarkdoc.dokka.util.tryCopy
-import org.jetbrains.dokka.model.DFunction
+import com.quarkdown.quarkdoc.dokka.transformers.QuarkdocParameterDocumentationTransformer
 import org.jetbrains.dokka.model.DParameter
 import org.jetbrains.dokka.model.DefaultValue
 import org.jetbrains.dokka.model.doc.Dl
 import org.jetbrains.dokka.model.doc.DocTag
 import org.jetbrains.dokka.model.doc.Li
-import org.jetbrains.dokka.model.doc.Param
 import org.jetbrains.dokka.model.doc.Text
 import org.jetbrains.dokka.model.doc.Ul
 import org.jetbrains.dokka.plugability.DokkaContext
@@ -23,33 +18,23 @@ import org.jetbrains.dokka.plugability.DokkaContext
  */
 class AdditionalParameterPropertiesTransformer(
     context: DokkaContext,
-) : QuarkdocDocumentableReplacerTransformer(context) {
-    private data class ParameterProperties(
+) : QuarkdocParameterDocumentationTransformer<AdditionalParameterPropertiesTransformer.ParameterProperties>(context) {
+    data class ParameterProperties(
         val isOptional: Boolean,
     )
 
-    private fun extractProperties(parameter: DParameter) =
+    override fun extractValue(parameter: DParameter) =
         ParameterProperties(
             isOptional = parameter.extra[DefaultValue] != null,
         )
 
     /**
-     * @return the parameters, among [parameters], that expect an enum value, associated with their enum declaration.
-     */
-    private fun associateParameters(parameters: List<DParameter>): Map<String, ParameterProperties> =
-        parameters
-            .asSequence()
-            .map { it.name to extractProperties(it) }
-            .filterNotNullEntries()
-            .toMap()
-
-    /**
      * @return the documentation content to add to the parameter documentation,
      * which lists the enum entries of the given [enum].
      */
-    private fun createNewDocumentationContent(properties: ParameterProperties): List<DocTag> =
+    override fun createNewDocumentation(value: ParameterProperties): List<DocTag> =
         buildList {
-            if (properties.isOptional) {
+            if (value.isOptional) {
                 this +=
                     Li(
                         listOf(
@@ -63,24 +48,8 @@ class AdditionalParameterPropertiesTransformer(
             )
         }
 
-    override fun transformFunction(function: DFunction): AnyWithChanges<DFunction> {
-        val properties: Map<String, ParameterProperties> =
-            associateParameters(function.parameters)
-                .takeIf { it.isNotEmpty() }
-                ?: return function.unchanged()
-
-        // Updates the documentation of the parameters to include the properties.
-        val documentation =
-            mapDocumentation(function.documentation) {
-                register(Param::class) { param ->
-                    val enum = properties[param.name] ?: return@register param
-                    val root = param.root
-                    param.copy(
-                        root = root.tryCopy(newChildren = createNewDocumentationContent(enum) + root.children),
-                    )
-                }
-            }
-
-        return function.copy(documentation = documentation).changed()
-    }
+    override fun mergeDocumentationContent(
+        old: List<DocTag>,
+        new: List<DocTag>,
+    ) = new + old
 }
