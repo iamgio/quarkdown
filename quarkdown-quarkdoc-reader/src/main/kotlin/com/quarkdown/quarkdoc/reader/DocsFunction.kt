0ddf761495739b571eb88f0ef431b8bae2c6f790
Giorgio Garofalo <giogar2705@gmail.com>
2025-06-08 02:00:50 +0200

    feat(quarkdoc-reader): add support to chaining

--- quarkdown-quarkdoc-reader/src/main/kotlin/com/quarkdown/quarkdoc/reader/DocsFunction.kt
+++ quarkdown-quarkdoc-reader/src/main/kotlin/com/quarkdown/quarkdoc/reader/DocsFunction.kt
@@ -1,6 +1,18 @@
 package com.quarkdown.quarkdoc.reader
 
 /**
+ * A function in a documentation file.
+ * @param name the name of the function
+ * @param parameters the parameters of the function
+ * @param isLikelyChained whether the function is likely to be chained
+ */
+data class DocsFunction(
+    val name: String,
+    val parameters: List<DocsParameter>,
+    val isLikelyChained: Boolean,
+)
+
+/**
  * A function parameter in a documentation file.
  * @param name the name of the parameter
  * @param description the description of the parameter, possibly in HTML format
