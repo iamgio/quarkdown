0c05a2fc23cb87e7d25cc926868353ce8ce5ae49
Giorgio Garofalo <giogar2705@gmail.com>
2025-06-22 12:58:52 +0200

    build(wrapper): don't override PUPPETEER_EXECUTABLE_PATH

--- scripts/puppeteer-executable.bat
+++ scripts/puppeteer-executable.bat
@@ -1,20 +1,21 @@
-setlocal
+@setlocal
 
-@rem Check common Windows paths for browsers
-if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" (
-    set "PUPPETEER_EXECUTABLE_PATH=%ProgramFiles%\Google\Chrome\Application\chrome.exe"
-) else if exist "%ProgramFiles%\Chromium\Application\chrome.exe" (
-    set "PUPPETEER_EXECUTABLE_PATH=%ProgramFiles%\Chromium\Application\chrome.exe"
-) else if exist "%ProgramFiles%\Mozilla Firefox\firefox.exe" (
-    set "PUPPETEER_EXECUTABLE_PATH=%ProgramFiles%\Mozilla Firefox\firefox.exe"
-    set "PUPPETEER_BROWSER=firefox"
-) else if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" (
-    set "PUPPETEER_EXECUTABLE_PATH=%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
-) else if exist "%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe" (
-    set "PUPPETEER_EXECUTABLE_PATH=%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe"
-    set "PUPPETEER_BROWSER=firefox"
-) else (
-    echo "[!] Could not find a suitable installation of Google Chrome, Chromium, or Firefox. PDF generation may not be available."
+@if not defined PUPPETEER_EXECUTABLE_PATH (
+    if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" (
+        set "PUPPETEER_EXECUTABLE_PATH=%ProgramFiles%\Google\Chrome\Application\chrome.exe"
+    ) else if exist "%ProgramFiles%\Chromium\Application\chrome.exe" (
+        set "PUPPETEER_EXECUTABLE_PATH=%ProgramFiles%\Chromium\Application\chrome.exe"
+    ) else if exist "%ProgramFiles%\Mozilla Firefox\firefox.exe" (
+        set "PUPPETEER_EXECUTABLE_PATH=%ProgramFiles%\Mozilla Firefox\firefox.exe"
+        set "PUPPETEER_BROWSER=firefox"
+    ) else if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" (
+        set "PUPPETEER_EXECUTABLE_PATH=%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
+    ) else if exist "%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe" (
+        set "PUPPETEER_EXECUTABLE_PATH=%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe"
+        set "PUPPETEER_BROWSER=firefox"
+    ) else (
+        echo "[!] Could not find a suitable installation of Google Chrome or Firefox. PDF generation may not be available."
+    )
 )
 
-endlocal
+@endlocal
