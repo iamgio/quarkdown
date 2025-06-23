9bda84c6699066dc918c342b9067d6cdd505919e
Giorgio Garofalo <giogar2705@gmail.com>
2025-06-23 11:10:10 +0200

    docs: update module not installed message

--- quarkdown-interaction/src/main/kotlin/com/quarkdown/interaction/executable/NodeModule.kt
+++ quarkdown-interaction/src/main/kotlin/com/quarkdown/interaction/executable/NodeModule.kt
@@ -9,17 +9,18 @@ open class NodeModule(
 )
 
 /**
- * Exception thrown when a required Node.js module is not installed globally.
+ * Exception thrown when a required Node.js module is not installed.
  * @param module the module that is not installed
  */
 class NodeModuleNotInstalledException(
     module: NodeModule,
 ) : IllegalStateException(
         """
-        Module '${module.name}' is not installed. Please install it via `npm install -g ${module.name}` and retry.
-        Make sure Quarkdown has the permissions to link the module correctly.
-        If your NPM package directory requires root privileges, please consider updating your NPM configuration.
-        For more information, see https://stackoverflow.com/questions/18088372/how-to-npm-install-global-not-as-root
+        Module '${module.name}' is not installed. Please install it via `npm install ${module.name} --prefix $${Env.npmGlobalPrefix}` and retry.
+        Make sure ${Env.npmGlobalPrefix} is an environment variable pointing to Quarkdown's `lib` directory or any other installation directory,
+        and it must be available at Quarkdown's launch.
+        
+        For more information, see: https://github.com/iamgio/quarkdown/wiki/pdf-export
         
         Note: installing Quarkdown via a package manager is suggested, as it sets up the required dependencies automatically.
         """.trimIndent(),
