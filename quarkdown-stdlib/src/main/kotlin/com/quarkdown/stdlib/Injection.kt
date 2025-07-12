aa4f1b9d7b5cc14750dbff948d9d6942d81fa8a1
Giorgio Garofalo <giogar2705@gmail.com>
2025-07-13 00:53:08 +0200

    feat(stdlib): add `.css` and `.cssproperties`

--- quarkdown-stdlib/src/main/kotlin/com/quarkdown/stdlib/Injection.kt
+++ quarkdown-stdlib/src/main/kotlin/com/quarkdown/stdlib/Injection.kt
@@ -4,6 +4,8 @@ import com.quarkdown.core.ast.base.block.Html
 import com.quarkdown.core.function.library.loader.Module
 import com.quarkdown.core.function.library.loader.moduleOf
 import com.quarkdown.core.function.reflect.annotation.LikelyBody
+import com.quarkdown.core.function.reflect.annotation.Name
+import com.quarkdown.core.function.value.Value
 import com.quarkdown.core.function.value.wrappedAsValue
 
 /**
@@ -13,10 +15,13 @@ import com.quarkdown.core.function.value.wrappedAsValue
 val Injection: Module =
     moduleOf(
         ::html,
+        ::css,
+        ::cssProperties,
     )
 
 /**
- * Creates an HTML element, which is rendered as-is without any additional processing or escaping.
+ * Creates an HTML element, which is rendered as-is without any additional processing or escaping,
+ * as long as the rendering target supports HTML.
  * @param content raw HTML content to inject
  * @return a new [Html] node
  * @wiki HTML
@@ -24,3 +29,62 @@ val Injection: Module =
 fun html(
     @LikelyBody content: String,
 ) = Html(content).wrappedAsValue()
+
+/**
+ * Creates a `<style>` HTML element with the provided CSS content.
+ * The content is wrapped in a `<style>` tag and rendered as-is,
+ * without any additional processing or escaping, as long as the rendering target supports HTML.
+ *
+ * Example:
+ * ```
+ * .css
+ *   body {
+ *     background-color: green !important;
+ *   }
+ * ```
+ *
+ * @param content raw CSS content to inject
+ * @return a new [Html] node representing the style element
+ * @see [cssProperties] for a more structured way to override CSS properties.
+ * @wiki CSS
+ */
+fun css(
+    @LikelyBody content: String,
+) = Html("<style data-hidden=\"\">$content</style>").wrappedAsValue()
+
+private const val CSS_ROOT_SELECTOR = ":root"
+private const val CSS_PROPERTY_PREFIX = "--qd-"
+
+/**
+ * Overrides the value of Quarkdown CSS properties.
+ * Each entry corresponds to a Quarkdown CSS property name and its value.
+ * The names will be prefixed with `--qd-` to match the Quarkdown CSS variable naming convention.
+ *
+ * For example:
+ * ```
+ * .cssproperties
+ *   - background-color: green
+ *   - main-font-size: 20px
+ * ```
+ *
+ * For a complete list of properties, see the [global theme](https://github.com/iamgio/quarkdown/blob/main/quarkdown-html/src/main/resources/render/theme/global.scss).
+ * Unknown properties will be ignored.
+ *
+ * The content is wrapped in a `<style>` tag and rendered as-is,
+ * without any additional processing or escaping, as long as the rendering target supports HTML.
+ * @param properties a map of CSS property names and their values
+ * @return a new [Html] node representing the style element
+ * @wiki CSS
+ */
+@Name("cssproperties")
+fun cssProperties(properties: Map<String, Value<*>>) =
+    css(
+        buildString {
+            append(CSS_ROOT_SELECTOR)
+            append(" { ")
+            properties.forEach { (name, value) ->
+                append("$CSS_PROPERTY_PREFIX$name: ${value.unwrappedValue} !important; ")
+            }
+            append("}")
+        },
+    )
