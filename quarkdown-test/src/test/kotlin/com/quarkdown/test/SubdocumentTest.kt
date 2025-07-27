8a3ba0a5d7158eea3939979385934847cafe4b16
Giorgio Garofalo <giogar2705@gmail.com>
2025-07-27 18:11:18 +0200

    feat(tests): add SubdocumentTest

--- quarkdown-test/src/test/kotlin/com/quarkdown/test/SubdocumentTest.kt
+++ quarkdown-test/src/test/kotlin/com/quarkdown/test/SubdocumentTest.kt
@@ -1,8 +1,13 @@
 package com.quarkdown.test
 
 import com.quarkdown.core.document.sub.Subdocument
+import com.quarkdown.core.pipeline.output.OutputResource
+import com.quarkdown.core.pipeline.output.TextOutputArtifact
 import com.quarkdown.test.util.execute
+import com.quarkdown.test.util.getSubResources
 import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertNotNull
 
 /**
  * Tests for subdocument generation.
@@ -10,14 +15,36 @@ import kotlin.test.Test
 class SubdocumentTest {
     private val subdocument = Subdocument("subdoc1", reader = { "Subdoc1".reader() })
 
+    private fun getResource(
+        group: OutputResource?,
+        subdocument: Subdocument,
+    ): TextOutputArtifact {
+        val resources = getSubResources(group)
+        val resource = resources.firstOrNull { it.name == subdocument.name } as? TextOutputArtifact
+        assertNotNull(resource)
+        return resource
+    }
+
     @Test
     fun `root to subdocument`() {
         execute(
             source = "",
             subdocumentGraph = { it.addVertex(subdocument).addEdge(Subdocument.ROOT, subdocument) },
-            outputResourceHook = {
-                println(it)
-                // TODO test the output dir
+            outputResourceHook = { group ->
+                val resource = getResource(group, subdocument)
+                assertContains(resource.content, "<html>")
+            },
+        ) {}
+    }
+
+    @Test
+    fun `context sharing to subdocument`() {
+        execute(
+            source = ".doctype {paged}",
+            subdocumentGraph = { it.addVertex(subdocument).addEdge(Subdocument.ROOT, subdocument) },
+            outputResourceHook = { group ->
+                val resource = getResource(group, subdocument)
+                assertContains(resource.content, "paged")
             },
         ) {}
     }
