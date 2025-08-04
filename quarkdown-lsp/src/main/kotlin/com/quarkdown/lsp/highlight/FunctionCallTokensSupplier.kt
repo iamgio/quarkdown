94e85e76cc29f947b0db6986bbd153c51c15afe8
Giorgio Garofalo <giogar2705@gmail.com>
2025-08-05 00:56:18 +0200

    feat(lsp): highlight nested function calls in arguments

--- quarkdown-lsp/src/main/kotlin/com/quarkdown/lsp/highlight/FunctionCallTokensSupplier.kt
+++ quarkdown-lsp/src/main/kotlin/com/quarkdown/lsp/highlight/FunctionCallTokensSupplier.kt
@@ -6,6 +6,11 @@ import com.quarkdown.core.lexer.regex.StandardRegexLexer
 import com.quarkdown.core.lexer.regex.pattern.TokenRegexPattern
 import org.eclipse.lsp4j.SemanticTokensParams
 
+private const val FUNCTION_CALL_BEGIN_TOKEN_NAME = "begin"
+private const val FUNCTION_CALL_IDENTIFIER_TOKEN_NAME = "identifier"
+private const val FUNCTION_CALL_PARAMETER_NAME_DELIMITER_TOKEN_NAME = "argumentNameDelimiter"
+private const val FUNCTION_CALL_ARGUMENT_CONTENT_TOKEN_NAME = "argContent"
+
 /**
  * Supplier for semantic tokens that highlight function calls.
  */
@@ -17,24 +22,53 @@ class FunctionCallTokensSupplier : SemanticTokensSupplier {
         val pattern: TokenRegexPattern = FunctionCallPatterns().inlineFunctionCall
         val lexer = StandardRegexLexer(text, listOf(pattern))
 
-        // Quarkdown tokens.
-        return lexer.tokenize().flatMap { token ->
-            val result = token.data.walkerResult ?: return@flatMap emptyList()
+        // When a function call argument is met, its content is enqueued
+        // and its semantic tokens are extracted at the end of the main tokenization.
+        val tokenizationQueue = mutableMapOf<Int, String>()
 
-            // Function call tokens are special, as they are processed by a walker
-            // which produces nested tokens for each part of the call (e.g. name and parameters).
-            // A semantic token is created for each eligible part.
-            result.tokens
-                .filter { it.offset <= result.endIndex }
-                .mapNotNull { match ->
-                    val start = token.data.position.first + match.offset
-                    SimpleTokenData(
-                        type = tokenToSemanticType(match) ?: return@mapNotNull null,
-                        range = start until (start + match.length),
-                    )
-                }.toList()
-        }
+        val tokens =
+            lexer.tokenize().flatMap { token ->
+                val result = token.data.walkerResult ?: return@flatMap emptyList()
+
+                // Function call are special tokens, as they are processed by a walker
+                // which produces nested tokens for each part of the call (e.g. name and parameters).
+                // A semantic token is created for each eligible part.
+                result.tokens
+                    .filter { it.offset <= result.endIndex }
+                    .mapNotNull { match ->
+                        val start = token.data.position.first + match.offset
+
+                        // Enqueuing the tokenization of function call arguments.
+                        if (match.type.name == FUNCTION_CALL_ARGUMENT_CONTENT_TOKEN_NAME) {
+                            tokenizationQueue += start to match.text
+                        }
+
+                        SimpleTokenData(
+                            type = tokenToSemanticType(match) ?: return@mapNotNull null,
+                            range = start until (start + match.length),
+                        )
+                    }.toList()
+            }
+
+        return tokens + extractEnqueuedTokens(params, tokenizationQueue)
     }
+
+    /**
+     * Extracts semantic tokens from the enqueued function call arguments.
+     * Each argument is tokenized separately, and the resulting tokens are adjusted
+     * to account for their original offset in the source text.
+     */
+    private fun extractEnqueuedTokens(
+        params: SemanticTokensParams,
+        queue: Map<Int, String>,
+    ): List<SimpleTokenData> =
+        queue.flatMap { (offset, text) ->
+            getTokens(params, text).map {
+                val start = it.range.start + offset
+                val end = it.range.last + offset
+                it.copy(range = start..end)
+            }
+        }
 
     /**
      * Produces a semantic token type based on the part of the function call,
@@ -42,9 +76,21 @@ class FunctionCallTokensSupplier : SemanticTokensSupplier {
      */
     private fun tokenToSemanticType(match: TokenMatch): TokenType? =
         when (match.type.name) {
-            "begin" -> TokenType.FUNCTION_CALL_IDENTIFIER
-            "identifier" -> if (match.tokenIndex == 1) TokenType.FUNCTION_CALL_IDENTIFIER else TokenType.FUNCTION_CALL_NAMED_PARAMETER
-            "argumentNameDelimiter" -> TokenType.FUNCTION_CALL_NAMED_PARAMETER
+            // .function
+            // ^
+            FUNCTION_CALL_BEGIN_TOKEN_NAME ->
+                TokenType.FUNCTION_CALL_IDENTIFIER
+
+            // .function parameter:{...}
+            //  ^^^^^^^^ ^^^^^^^^^ (depending on the index)
+            FUNCTION_CALL_IDENTIFIER_TOKEN_NAME ->
+                if (match.tokenIndex == 1) TokenType.FUNCTION_CALL_IDENTIFIER else TokenType.FUNCTION_CALL_NAMED_PARAMETER
+
+            // .function parameter:{...}
+            //                    ^
+            FUNCTION_CALL_PARAMETER_NAME_DELIMITER_TOKEN_NAME ->
+                TokenType.FUNCTION_CALL_NAMED_PARAMETER
+
             else -> null
         }
 }
