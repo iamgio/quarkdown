7be5f882ab8d194b711909d38fe74d5d178de0fd
Giorgio Garofalo <giogar2705@gmail.com>
2025-09-22 01:00:55 +0200

    test(cross-reference): test cross-references

---
+++ quarkdown-core/src/test/kotlin/com/quarkdown/core/CrossReferenceResolutionTest.kt
@@ -0,0 +1,134 @@
+package com.quarkdown.core
+
+import com.quarkdown.core.ast.NestableNode
+import com.quarkdown.core.ast.Node
+import com.quarkdown.core.ast.attributes.reference.getDefinition
+import com.quarkdown.core.ast.base.block.Heading
+import com.quarkdown.core.ast.dsl.buildBlock
+import com.quarkdown.core.ast.dsl.buildInline
+import com.quarkdown.core.ast.iterator.ObservableAstIterator
+import com.quarkdown.core.ast.quarkdown.reference.CrossReference
+import com.quarkdown.core.context.MutableContext
+import com.quarkdown.core.context.hooks.location.LocationAwareLabelStorerHook
+import com.quarkdown.core.context.hooks.location.LocationAwarenessHook
+import com.quarkdown.core.context.hooks.reference.CrossReferenceResolverHook
+import com.quarkdown.core.flavor.quarkdown.QuarkdownFlavor
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+private const val ID = "ref-id"
+
+/**
+ * Tests for resolving cross-references to their definitions.
+ */
+class CrossReferenceResolutionTest {
+    private val context = MutableContext(QuarkdownFlavor)
+
+    private fun traverse(root: Node) {
+        ObservableAstIterator()
+            .attach(LocationAwarenessHook(context))
+            .attach(LocationAwareLabelStorerHook(context))
+            .attach(CrossReferenceResolverHook(context))
+            .traverse(root as NestableNode)
+    }
+
+    @Test
+    fun `reference after definition (heading)`() {
+        val definition = Heading(depth = 2, text = buildInline { text("Heading") }, customId = ID)
+        val reference = CrossReference(ID)
+        val root =
+            buildBlock {
+                root {
+                    +definition
+                    +reference
+                }
+            }
+
+        traverse(root)
+        assertEquals(definition, reference.getDefinition(context))
+    }
+
+    @Test
+    fun `reference before definition (heading)`() {
+        val definition = Heading(depth = 6, text = buildInline { text("Heading") }, customId = ID)
+        val reference = CrossReference(ID)
+        val root =
+            buildBlock {
+                root {
+                    +reference
+                    +definition
+                }
+            }
+
+        traverse(root)
+        assertEquals(definition, reference.getDefinition(context))
+    }
+
+    @Test
+    fun `invalid reference`() {
+        val reference = CrossReference(ID)
+        val root = buildBlock { root { +reference } }
+
+        traverse(root)
+        assertNull(reference.getDefinition(context))
+    }
+
+    @Test
+    fun `multiple references to the same definition`() {
+        val definition = Heading(depth = 3, text = buildInline { text("Heading") }, customId = ID)
+        val reference1 = CrossReference(ID)
+        val reference2 = CrossReference(ID)
+        val root =
+            buildBlock {
+                root {
+                    +reference1
+                    +definition
+                    +reference2
+                }
+            }
+
+        traverse(root)
+        assertEquals(definition, reference1.getDefinition(context))
+        assertEquals(definition, reference2.getDefinition(context))
+    }
+
+    @Test
+    fun `multiple definitions with the same ID - first one wins`() {
+        val definition1 = Heading(depth = 4, text = buildInline { text("Heading 1") }, customId = ID)
+        val definition2 = Heading(depth = 5, text = buildInline { text("Heading 2") }, customId = ID)
+        val reference = CrossReference(ID)
+        val root =
+            buildBlock {
+                root {
+                    +definition1
+                    +reference
+                    +definition2
+                }
+            }
+
+        traverse(root)
+        assertEquals(definition1, reference.getDefinition(context))
+    }
+
+    @Test
+    fun `mutual references (headings)`() {
+        val definition1 = Heading(depth = 2, text = buildInline { text("Heading 1") }, customId = "id-1")
+        val definition2 = Heading(depth = 3, text = buildInline { text("Heading 2") }, customId = "id-2")
+        val reference1 = CrossReference("id-2")
+        val reference2 = CrossReference("id-1")
+        val root =
+            buildBlock {
+                root {
+                    +definition1
+                    +reference1
+                    +definition2
+                    +reference2
+                }
+            }
+
+        traverse(root)
+        assertEquals(definition2, reference1.getDefinition(context))
+        assertEquals(definition1, reference2.getDefinition(context))
+    }
+}
---
+++ quarkdown-test/src/test/kotlin/com/quarkdown/test/CrossReferenceTest.kt
@@ -0,0 +1,151 @@
+package com.quarkdown.test
+
+import com.quarkdown.test.util.DEFAULT_OPTIONS
+import com.quarkdown.test.util.execute
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/**
+ * Tests for cross-references.
+ */
+class CrossReferenceTest {
+    @Test
+    fun `invalid reference`() {
+        execute("See .ref {x}") {
+            assertEquals("<p>See [???]</p>", it)
+        }
+    }
+
+    @Test
+    fun `reference after definition (heading)`() {
+        execute(
+            """
+            ## Title {#my-ref}
+            
+            See .ref {my-ref}.
+            """.trimIndent(),
+        ) {
+            assertEquals(
+                "<h2 id=\"my-ref\">Title</h2>" +
+                    "<p>See <a href=\"#my-ref\"><span class=\"cross-reference\">Title</span></a>.</p>",
+                it,
+            )
+        }
+    }
+
+    @Test
+    fun `reference before definition (heading)`() {
+        execute(
+            """
+            See .ref {my-ref}.
+            
+            ## Title {#my-ref}
+            """.trimIndent(),
+        ) {
+            assertEquals(
+                "<p>See <a href=\"#my-ref\"><span class=\"cross-reference\">Title</span></a>.</p>" +
+                    "<h2 id=\"my-ref\">Title</h2>",
+                it,
+            )
+        }
+    }
+
+    @Test
+    fun `multiple references to the same definition (heading)`() {
+        execute(
+            """
+            See .ref {my-ref} and .ref {my-ref}.
+            
+            ## Title {#my-ref}
+            """.trimIndent(),
+        ) {
+            assertEquals(
+                "<p>See <a href=\"#my-ref\"><span class=\"cross-reference\">Title</span></a> and " +
+                    "<a href=\"#my-ref\"><span class=\"cross-reference\">Title</span></a>.</p>" +
+                    "<h2 id=\"my-ref\">Title</h2>",
+                it,
+            )
+        }
+    }
+
+    @Test
+    fun `mutual references (heading)`() {
+        execute(
+            """
+            See .ref {ref-a}.
+            
+            ## Title A {#ref-a}
+            
+            See also .ref {ref-b}.
+            
+            ## Title B {#ref-b}
+            
+            Back to .ref {ref-a}.
+            """.trimIndent(),
+        ) {
+            assertEquals(
+                "<p>See <a href=\"#ref-a\"><span class=\"cross-reference\">Title A</span></a>.</p>" +
+                    "<h2 id=\"ref-a\">Title A</h2>" +
+                    "<p>See also <a href=\"#ref-b\"><span class=\"cross-reference\">Title B</span></a>.</p>" +
+                    "<h2 id=\"ref-b\">Title B</h2>" +
+                    "<p>Back to <a href=\"#ref-a\"><span class=\"cross-reference\">Title A</span></a>.</p>",
+                it,
+            )
+        }
+    }
+
+    @Test
+    fun `numbered references (heading)`() {
+        execute(
+            """
+            .noautopagebreak
+            .numbering
+                - headings: 1.1
+            
+            See .ref {first-ref} and .ref {second-ref}.
+            
+            # Title {#first-ref}
+            
+            ## Subitle {#second-ref}
+            """.trimIndent(),
+            DEFAULT_OPTIONS.copy(enableLocationAwareness = true),
+        ) {
+            assertEquals(
+                "<p>See <a href=\"#first-ref\"><span class=\"cross-reference\" data-location=\"1\"></span></a>" +
+                    " and <a href=\"#second-ref\"><span class=\"cross-reference\" data-location=\"1.1\"></span></a>.</p>" +
+                    "<h1 id=\"first-ref\" data-location=\"1\">Title</h1>" +
+                    "<h2 id=\"second-ref\" data-location=\"1.1\">Subitle</h2>",
+                it,
+            )
+        }
+    }
+
+    @Test
+    fun `localized numbered references (heading)`() {
+        execute(
+            """
+            .noautopagebreak
+            .doclang {en}
+            .numbering
+                - headings: 1.1
+            
+            See .ref {first-ref} and .ref {second-ref}.
+            
+            # Title {#first-ref}
+            
+            ## Subitle {#second-ref}
+            """.trimIndent(),
+            DEFAULT_OPTIONS.copy(enableLocationAwareness = true),
+        ) {
+            assertEquals(
+                "<p>See <a href=\"#first-ref\">" +
+                    "<span class=\"cross-reference\" data-location=\"1\" data-localized-kind=\"Section\"></span></a>" +
+                    " and <a href=\"#second-ref\">" +
+                    "<span class=\"cross-reference\" data-location=\"1.1\" data-localized-kind=\"Section\"></span></a>.</p>" +
+                    "<h1 id=\"first-ref\" data-location=\"1\">Title</h1>" +
+                    "<h2 id=\"second-ref\" data-location=\"1.1\">Subitle</h2>",
+                it,
+            )
+        }
+    }
+}
