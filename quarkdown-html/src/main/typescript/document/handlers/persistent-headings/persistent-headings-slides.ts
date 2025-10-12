e03c0d45c5ee6a9a309b3b8c5b4460bdc4059c2e
Giorgio Garofalo <giogar2705@gmail.com>
2025-10-12 20:11:26 +0200

    fix: fix persistent headings in PDF slides

--- quarkdown-html/src/main/typescript/document/handlers/persistent-headings/persistent-headings-slides.ts
+++ quarkdown-html/src/main/typescript/document/handlers/persistent-headings/persistent-headings-slides.ts
@@ -8,13 +8,13 @@ export class PersistentHeadingsSlides extends PersistentHeadingsDocumentHandler 
      * Post-rendering hook that applies persistent headings to both the slide and background elements.
      */
     async onPostRendering() {
-        const slides = document.querySelectorAll('.reveal .slides > :is(section, div)');
-        const backgrounds = document.querySelectorAll('.reveal .backgrounds > .slide-background');
+        const slides = document.querySelectorAll('.reveal .slides > :is(section, .pdf-page)');
+        const backgrounds = document.querySelectorAll('.reveal > :is(.backgrounds, .pdf-page) > .slide-background');
 
         slides.forEach((slide, index) => {
             this.apply({
                 sourceContainer: slide,
-                targetContainers: [slide, backgrounds[index]],
+                targetContainers: [slide, backgrounds[index]].filter(Boolean),
             });
         });
     }
