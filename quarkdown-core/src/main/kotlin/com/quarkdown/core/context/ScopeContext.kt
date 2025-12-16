b7527448193ece0009ac8dd1063401b39460dd8f
Giorgio Garofalo <giogar2705@gmail.com>
2025-12-15 21:05:03 -0800

    fix: fix wrong SubdocumentContext filesystem

--- quarkdown-core/src/main/kotlin/com/quarkdown/core/context/ScopeContext.kt
+++ quarkdown-core/src/main/kotlin/com/quarkdown/core/context/ScopeContext.kt
@@ -6,15 +6,16 @@ import com.quarkdown.core.context.file.FileSystem
  * A context that is the result of a fork from an original parent [Context].
  * All properties are inherited from it, but not all, such as libraries, are shared mutably.
  * @param parent context this scope was forked from
- * @param fileSystem file system to use in this context
+ * @param fileSystem file system to use in this context. Overrides the attached pipeline's file system if provided
  * @see SubdocumentContext to see what's inherited from the parent context
  */
 open class ScopeContext(
     parent: MutableContext,
-    override val fileSystem: FileSystem = parent.fileSystem,
+    fileSystem: FileSystem? = null,
 ) : SubdocumentContext(
         parent = parent,
         subdocument = parent.subdocument,
+        _fileSystem = fileSystem,
     ) {
     // A scope context shares the parent's document info.
     override var documentInfo by parent::documentInfo
