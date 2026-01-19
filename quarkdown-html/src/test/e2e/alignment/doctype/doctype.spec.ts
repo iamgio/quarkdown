c615d5afe4dedf13e6b315ffffa1bd8b17893432
Giorgio Garofalo <giogar2705@gmail.com>
2026-01-18 23:07:42 -0800

    test(e2e): test doctype alignment

---
+++ quarkdown-html/src/test/e2e/alignment/doctype/doctype.spec.ts
@@ -0,0 +1,35 @@
+import {suite} from "../../quarkdown";
+
+const {testMatrix, expect} = suite(__dirname);
+
+testMatrix(
+    "applies correct text alignment per doctype",
+    ["plain", "paged", "slides", "docs"],
+    async (page, docType) => {
+        const heading = page.locator("h1").first();
+        const paragraph = page.locator("p").first();
+        const listItem = page.locator("li").first();
+        const inlineMath = page.locator("p > formula").first();
+        const blockMath = page.locator("formula[data-block]").first();
+
+        let alignmentGlobal = "start";
+        let alignmentLocal = "start";
+
+        if (docType === "slides") {
+            alignmentGlobal = "center";
+            alignmentLocal = "center";
+        } else if (docType === "plain" || docType === "paged") {
+            alignmentLocal = "justify";
+        }
+
+        await expect(heading).toHaveCSS("text-align", alignmentGlobal);
+        await expect(paragraph).toHaveCSS("text-align", alignmentLocal);
+        await expect(paragraph).toHaveCSS("text-align-last", alignmentGlobal);
+        await expect(listItem).toHaveCSS("text-align", alignmentLocal);
+        await expect(listItem).toHaveCSS("text-align-last", "start");
+        await expect(inlineMath).toHaveCSS("text-align", "start");
+        await expect(inlineMath).toHaveCSS("text-align-last", "auto");
+        await expect(blockMath).toHaveCSS("text-align", "start");
+        await expect(blockMath).toHaveCSS("text-align-last", "auto");
+    }
+);
