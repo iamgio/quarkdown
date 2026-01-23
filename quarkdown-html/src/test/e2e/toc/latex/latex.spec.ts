b1052adf136bb3ceadf7057920ed66b70f9ea07a
Giorgio Garofalo <giogar2705@gmail.com>
2026-01-22 21:47:51 -0800

    fix(theme/latex): fix toc font weight in docs

--- quarkdown-html/src/test/e2e/toc/latex/latex.spec.ts
+++ quarkdown-html/src/test/e2e/toc/latex/latex.spec.ts
@@ -19,3 +19,19 @@ test("displays correct numbering in li::before", async (page) => {
 test("links to correct heading anchors", async (page) => {
     await assertTocLinks(page);
 });
+
+test("first-level links are bold, others are normal", async (page) => {
+    const nav = page.locator("nav");
+    const firstLevelLinks = nav.locator("> :is(ul, ol) > li > a");
+    const nestedLinks = nav.locator(":is(ul, ol) :is(ul, ol) a");
+
+    // First-level links are bold
+    for (const link of await firstLevelLinks.all()) {
+        await expect(link).toHaveCSS("font-weight", "700");
+    }
+
+    // Nested links are normal
+    for (const link of await nestedLinks.all()) {
+        await expect(link).toHaveCSS("font-weight", "400");
+    }
+});
